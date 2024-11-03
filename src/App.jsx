94e56fcef3597f5ab47f67db9b58e835f1faac02
gerynsb <118332285+gerynsb@users.noreply.github.com>
2024-11-03 20:28:46 +0800

    Update : NNN day 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header'
 import Slider from './Components/Slider'
+import ProductionHouse from './Components/productionHouse'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,10 @@ function App() {
     <>
       <div>
       <Header/>
+
       <Slider/>
+
+      <ProductionHouse/>
       </div>
     </>
   )
