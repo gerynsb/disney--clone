9e3d3666f426599137b2b43fb33ab2fa785738eb
gerynsb <118332285+gerynsb@users.noreply.github.com>
2024-11-05 05:38:56 +0800

    update : gagal 1 hari NNN

--- src/Components/productionHouse.jsx
+++ src/Components/productionHouse.jsx
@@ -42,9 +42,18 @@ function productionHouse() {
 
     ]
   return (
-    <div>
+    <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16'>
         {productionHouseList.map((item)=>(
-            <div></div>
+            <div className='border-[2px] border-gray-600
+            rounded-lg hover:scale-110 transition-all duration-300
+            ease-in-out cursor-pointer relative shadow-x1 
+            shadow-gray
+            '>
+                <video src={item.video} autoPlay loop playsInline
+                className='absolute top-0 rounded-md z-0 opacity-0
+                hover:opacity-50'/>
+                <img src={item.image} className='w-full z-[1]'/>
+            </div>
         ))}
     </div>
   )
