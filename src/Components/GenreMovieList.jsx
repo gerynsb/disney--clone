b26e475c9b461041e27c5783d2b8fc26c0ef7f49
gerynsb <118332285+gerynsb@users.noreply.github.com>
2024-11-06 11:29:03 +0800

    Update : NNN again start over

---
+++ src/Components/GenreMovieList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function GenreMovieList() {
+  return (
+    <div>
+        {GenresList.genere.map((item)=>(
+            
+        ))}
+    </div>
+  )
+}
+
+GenreMovieList.propTypes = {}
+
+export default GenreMovieList
--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -2,10 +2,15 @@
 
     const movieBaseUrl='https://api.themoviedb.org/3'
     const api_key = '3213ea69130f58a230ae93a5bb019918'
-    //https://api.themoviedb.org/3/trending/all/day?api_key=3213ea69130f58a230ae93a5bb019918
 
+
+    const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf';
+
+    //https://api.themoviedb.org/3/trending/all/day?api_key=3213ea69130f58a230ae93a5bb019918
     const getTrendingVideos=axios.get(`${movieBaseUrl}/trending/all/day?api_key=${api_key}`);
+    const getMoviesByGenreId = (id) => axios.get(getMoviesByGenreId+"&with_genres="+id)
 
     export default{
-        getTrendingVideos
+        getTrendingVideos,
+        getMoviesByGenreId
     }
