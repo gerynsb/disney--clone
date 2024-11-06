2a7a48bb6ff69fea5d4f571ee61b65a194401d4e
gerynsb <118332285+gerynsb@users.noreply.github.com>
2024-11-06 11:39:44 +0800

    Update : NNN again

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './Components/Header'
 import Slider from './Components/Slider'
 import ProductionHouse from './Components/productionHouse'
+import GenreMovieList from './Components/GenreMovieList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,8 @@ function App() {
       <Slider/>
 
       <ProductionHouse/>
+
+      <GenreMovieList/>
       </div>
     </>
   )
--- src/Components/GenreMovieList.jsx
+++ src/Components/GenreMovieList.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import GenresList from '../Constant/GenreList'
 
 function GenreMovieList() {
   return (
     <div>
-        {GenresList.genere.map((item)=>(
+        {GenresList.genere.map((item, index)=>index<=4&&(
             <div>
-                <h2>{item.name}</h2>
+                <h2 className='text=[20px] text-white font-bold'>{item.name}</h2>
             </div>
         ))}
     </div>
